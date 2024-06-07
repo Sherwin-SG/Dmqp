e57927195de820b0657d678a597eaddf8b1aa55b
Sherwin-SG <129164340+Sherwin-SG@users.noreply.github.com>
2024-06-07 17:12:25 +0530

    Update delete_employee.php

--- delete_employee.php
+++ delete_employee.php
@@ -12,12 +12,12 @@ if ($conn->connect_error) {
 if (isset($_GET['employee_no'])) {
     $employeeNo = $_GET['employee_no'];
 
-    // Delete the employee record from the database
+    
     $deleteSql = "DELETE FROM employee WHERE employee_no = '$employeeNo'";
     if ($conn->query($deleteSql) === TRUE) {
         echo "Employee deleted successfully";
 
-        // Redirect to employee.php
+         
         header("Location: employee.php");
         exit();
     } else {
