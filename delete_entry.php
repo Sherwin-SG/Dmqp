4ffa10e4306f67f6dd8584257a9659c911b4eabb
Sherwin-SG <129164340+Sherwin-SG@users.noreply.github.com>
2024-06-07 17:12:42 +0530

    Update delete_entry.php

--- delete_entry.php
+++ delete_entry.php
@@ -1,17 +1,17 @@
 <?php
-include('db_connect.php'); // Include your database connection file
+include('db_connect.php');  
 
 if (isset($_POST['ref_no'])) {
     $ref_no = $_POST['ref_no'];
 
-    // Perform the deletion query
+     
     $delete_query = "DELETE FROM payroll WHERE ref_no = ?";
     $stmt = $conn->prepare($delete_query);
-    $stmt->bind_param('s', $ref_no); // Assuming ref_no is a varchar, adjust if needed
+    $stmt->bind_param('s', $ref_no);  
     $stmt->execute();
     $stmt->close();
 
-    // Check if the deletion was successful
+    
     if ($conn->affected_rows > 0) {
         echo "Entry with Ref_No $ref_no deleted successfully.";
     } else {
